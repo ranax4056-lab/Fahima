421db9c1261589214a43a301b7ac25d92715b113
ranax4056-lab <ranax4056@gmail.com>
2026-05-13 09:18:56 +0600

    Update botName in settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
   packname: '*⎯͢✧🫣 𝐒ʜꫝʜɪɴᥫ᭡ 🐱*',
   author: '‎',
-  botName: "*⎯͢✧🫣 𝐒ʜꫝʜɪɴᥫ᭡ 🐱*",
+  botName: "*⎯͢✧🫣 𝐒ʜꫝʜɪɴ 𝐁ᴏᴛ*🐱",
   botOwner: '*⎯͢✧😌 𝐒ʜꫝʜɪɴ 𝐑ᴀɴꫝᥫ᭡ 🐱*', // Your name
   ownerNumber: '8801742564047', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
